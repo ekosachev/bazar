c708932fef69e9495329d189505f59b00df659e3
Egor Kosachev <egorkosachev@vk.com>
2026-07-10 01:21:39 +0700

    add basic chat handler

---
+++ back/internal/chat/handler.go
@@ -0,0 +1,54 @@
+package chat
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/ekosachev/bazar/internal/middleware"
+	"github.com/ekosachev/bazar/internal/utils"
+	"github.com/gin-gonic/gin"
+)
+
+type ChatHandler struct {
+	service *ChatService
+}
+
+func NewChatHandler(service *ChatService) *ChatHandler {
+	return &ChatHandler{service: service}
+}
+
+func (h *ChatHandler) RegisterRoutes(group gin.RouterGroup) {
+	chatGroup := group.Group("/chat")
+	{
+		accessTokenGroup := chatGroup.Group("/").Use(middleware.RequiresAccessToken())
+		{
+			accessTokenGroup.POST("/", h.create)
+		}
+	}
+}
+
+func (h *ChatHandler) create(c *gin.Context) {
+	var req CreateChatRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		utils.SendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	response, err := h.service.Create(c, req, c.GetString("userID"))
+	if err != nil {
+		var invalidChatType *ErrInvalidChatType
+		if errors.As(err, &invalidChatType) {
+			utils.SendError(c, http.StatusBadRequest, err.Error())
+			return
+		}
+
+		utils.SendError(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, utils.APIResponse{
+		Success: true,
+		Data:    response,
+	})
+}
